ba0155a6a2d03f4ea3b7293815f5548219237555
KJohn <mayacafe123@gmail.com>
2021-07-29 23:45:29 +0800

    todoList-context

---
+++ components/TodoList/todoList-context.js
@@ -0,0 +1,62 @@
+import React, { useReducer } from 'react';
+import PropTypes from 'prop-types';
+
+const todoListContext = React.createContext();
+
+const initialState = {
+    todoList: []
+};
+
+const actions = {
+    ADD_TODO_ITEM: "ADD_TODO_ITEM",
+    REMOVE_TODO_ITEM: "REMOVE_TODO_ITEM",
+    TOGGLE_COMPLETED: "TOGGLE_COMPLETED"
+};
+
+const reducer = (state, action) => {
+    console.log(action.type);
+    switch (action.type) {
+      case actions.ADD_TODO_ITEM:
+        return {
+          todoList: [
+            ...state.todoList,
+            {
+              id: new Date().valueOf(),
+              label: action.todoItemLabel,
+              completed: false
+            }
+          ]
+        };
+      case actions.REMOVE_TODO_ITEM: {
+        return { todoList: state.todoList.filter((todoItem) => todoItem.id !== action.todoItemId) };
+      }
+      case actions.TOGGLE_COMPLETED: {
+        return { todoList: state.todoList.map((todoItem) =>
+            todoItem.id === action.todoItemId
+              ? { ...todoItem, completed: !todoItem.completed }
+              : todoItem
+        )};
+      }
+      default:
+        throw new Error(`Unhandled action type: ${action.type}`);
+    }
+};
+
+const TodoListProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const value = {state, dispatch};
+    return <todoListContext.Provider value={value}>{children}</todoListContext.Provider>;
+};
+
+TodoListProvider.propTypes = {
+    children: PropTypes.node.isRequired
+};
+
+const useTodo = () => {
+    const context = React.useContext(todoListContext);
+    if (context === undefined) throw new Error('useTodo must be used within a TodoListProvider');
+    return context;
+};
+
+export { TodoListProvider, useTodo };
+
