4478a0ddc629093fc8b726e3bc5634455c6d12e2
KJohn <mayacafe123@gmail.com>
2021-07-30 16:16:32 +0800

    page styles and task count

--- components/TodoList/TodoList.jsx
+++ components/TodoList/TodoList.jsx
@@ -8,7 +8,15 @@ export default function TodoList() {
   const { state: {todoList} } = useTodo();
   return (
     <div className={styles.root}>
-      <ul>
+      <h3 className={styles.title}>Todo List</h3>
+
+      <div className={styles.taskCount}>
+        <p className={styles.total}>{`${todoList.length} tasks`}</p>
+        <p className={styles.done}>{`${todoList.filter((todoItem) => todoItem.isCompleted === true).length} done`}</p>
+        <p className={styles.togo}>{`${todoList.filter((todoItem) => todoItem.isCompleted === false).length} togo`}</p>
+      </div>
+      
+      <ul className={styles.list}>
         {
           todoList.map((todoItem) => <TodoItem key={todoItem.id} todoItem={todoItem} />)
         }
