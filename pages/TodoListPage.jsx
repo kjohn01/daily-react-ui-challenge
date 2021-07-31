3af4cf9411f1aea8f34769c5dd9e0deb8e43d7b1
KJohn <mayacafe123@gmail.com>
2021-08-01 00:52:19 +0800

    handle input overflow & cancel editing onBlur

--- pages/TodoListPage.jsx
+++ pages/TodoListPage.jsx
@@ -5,15 +5,18 @@ import TodoList from '../components/TodoList/containers/TodoList';
 import { TodoListProvider } from '../components/TodoList/todoList-context';
 import styles from './todoListPage.module.scss';
 
-const SignUpPage = () => {
+const TodoListPage = () => {
     return (
       <TodoListProvider> 
         <div className={styles.root}>
           <BackButton />
           <TodoList />
-          <Footer link="https://dribbble.com/shots/3967195-ToDo-Task-List" author="Vishnu Prasad" />
+          <Footer 
+            text="Design inspired by"
+            link="https://dribbble.com/shots/3967195-ToDo-Task-List" author="Vishnu Prasad" 
+          />
         </div>
       </TodoListProvider>
     )
 }
-export default SignUpPage;
+export default TodoListPage;
